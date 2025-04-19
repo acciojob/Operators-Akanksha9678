cdfd96e87158f378e137cc8602e8cdd61a534827
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-19 20:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+// let num1=document.querySelector("#num1").value;
+// let num2=document.querySelector("#num2").value;
+let btn=document.querySelector("#calculate");
+let output=document.querySelector("#result");
+
+btn.addEventListener("click",()=>{
+let result=0;
+let num1=document.querySelector("#num1").value;
+let num2=document.querySelector("#num2").value;
+let operator=document.querySelector("#operator");	
+
+	switch(operator){
+		case '+':
+			result=num1+num2;
+		case '-':
+			result=num1-num2;
+		case '*':
+			result=num1*num2;
+		case '/':
+			result=num1/num2;
+	}
+})
